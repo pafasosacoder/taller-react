d088695cf1f734f31219cd1ab6341a07c2f308d8
Pablo Solorzano <pablo.solorzano@alquimiasoft.com.ec>
2021-03-01 10:08:03 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+// import logo from './logo.svg'a;
+// import './App.css';
+import { useState } from "react";
+import Header from './components/Header';
+import Tasks from "./components/Tasks";
+
+
+const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+      "id": 1,
+      "text": "Doctors Appointment",
+      "day": "Feb 5th at 2:30pm",
+      "reminder": true
+    },
+    {
+      "id": 2,
+      "text": "Meeting at School",
+      "day": "Feb 6th at 1:30pm",
+      "reminder": true
+    }
+  ]);
+
+  //Delete task
+  const deleteTask = (id) => {
+    console.log('delete ', id)
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle reminder
+
+  const toggleReminder = (id) => {
+    console.log(id)
+  }
+
+  return (
+    <div className='container'>
+      <Header title='pablo' />
+      { tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ( 'No Tasks to show' )}
+    </div>
+  );
+}
+
+
+export default App;
