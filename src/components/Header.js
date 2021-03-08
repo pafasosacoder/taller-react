2b80acdd54477607ccc71a10d1d87b02ab1b7773
Pablo Solorzano <pablo.solorzano@alquimiasoft.com.ec>
2021-03-07 20:47:06 -0500

    ultimos cambios

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import PropTypes from 'prop-types'
 import Button from "./Button";
+import { useLocation } from 'react-router-dom';
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAdd }) => {
 
-    const onClick = () => {
-        console.log('click')
-    }
-
+    const location = useLocation();
     return (
         <header className='header'>
-            <h1>{ title }</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
+            <h1>{title}</h1>
+            {location.pathname === '/' &&
+                <Button
+                    color={showAdd ? 'red' : 'green'}
+                    text={showAdd ? 'Close' : 'Add'}
+                    onClick={onAdd} />}
         </header>
     )
 }
